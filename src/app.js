310e6fbad82447f3e4119a3e6efb46800a41dde9
Nora Sandler <gulnora.touraeva@gmail.com>
2020-10-30 23:08:49 -0400

    Added auth,migration, error-handler

--- src/app.js
+++ src/app.js
@@ -3,15 +3,27 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
+const {
+    NODE_ENV
+} = require('./config')
+const errorHandler = require('./middleware/error-handler')
+const pancakeRouter = require('./pancake/pancake-router')
 
 const app = express()
 
-const morganOption = (process.env.NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
+const morganOption = (NODE_ENV === 'production') ?
+    'tiny' :
+    'common';
 
-app.use(morgan(morganOption))
-app.use(helmet())
+app.use(morgan(morganOption, {
+    skip: () => NODE_ENV === 'test',
+}))
 app.use(cors())
+app.use(helmet())
+
+app.use(express.static('public'))
+
+app.use('/api/pancakes', pancakeRouter)
+app.use(errorHandler)
 
 module.exports = app
